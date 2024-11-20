926d9ad21a07182b631abfa8a0c96f20369374ba
Extager <164411975+Extager@users.noreply.github.com>
2024-11-20 16:40:38 +0300

    about_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./compontents/header/header.jsx";
 import About_1 from "./compontents/main/about_1/about_1.jsx";
+import About_2 from './compontents/main/about_2/about_2.jsx';
 
 export default function App(){
   return(
@@ -9,6 +10,9 @@ export default function App(){
     </div>
     <div>
       <About_1 />
+    </div>
+    <div>
+      <About_2 />
     </div>
     </>
     
---
+++ src/compontents/main/about_2/about_2.jsx
@@ -0,0 +1,51 @@
+import Star from '../../../images/Star.svg';
+import Lamp from '../../../images/Lamp.svg';
+import Сhemistry from '../../../images/Сhemistry.svg';
+import Museum from '../../../images/Museum.svg';
+
+export default function About_2(){
+    return(
+        <div className="w-screen h-[60vh] flex flex-col justify-center max-sm:h-auto bg-[#f2f7f7]">
+            <div className="font-medium text-4xl text-center max-sm:text-3xl my-10">
+                <p>Погружение в космос рядом с домом</p>
+            </div>
+
+            <div className="grid grid-cols-2 justify-items-center mx-auto max-sm:grid-cols-1">
+                <div className="flex flex-row my-10 max-w-[460px] text-pretty max-sm:text-balance mx-10 max-lg:mx-0">
+                    <img src={Star} alt="Image Error" className='w-[60px] h-[60px]'/>
+                    <div className="flex flex-col ml-5">
+                        <p className='text-xl'>Звёздное небо над головой</p>
+                        <p  className='font-light text-base'>Вы сможете наблюдать за небесными явлениями и изучать созвездия.</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-row my-10 max-w-[460px] text-pretty max-sm:text-balance">
+                    <img src={Lamp} alt="Image Error" className='w-[60px] h-[60px]'/>
+                    <div className="flex flex-col ml-5">
+                        <p className='text-xl'>Захватывающие проекции</p>
+                        <p  className='font-light text-base'>С помощью проекций вы сможете увидеть красоту Вселенной.</p>
+                    </div>
+
+                </div>
+
+                <div className="flex flex-row my-10 max-w-[460px] text-pretty max-sm:text-balance">
+                    <img src={Museum} alt="Image Error" className='w-[60px] h-[60px]'/>
+                    <div className="flex flex-col ml-5">
+                        <p className='text-xl'>Интерактивные экспонаты</p>
+                        <p className='font-light text-base'>Экспозиция планетария включает увлекательные интерактивные экспонаты.</p>
+                    </div>
+                
+                </div>
+
+                <div className="flex flex-row my-10 max-w-[460px] text-pretty max-sm:text-balance">
+                    <img src={Сhemistry} alt="Image Error" className='w-[60px] h-[60px]'/>
+                    <div className="flex flex-col ml-5">
+                        <p className='text-xl'>Научные открытия</p>
+                        <p className='font-light text-base'>На экскурсиях вы узнаете о последних научных открытиях в области астрономии.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
